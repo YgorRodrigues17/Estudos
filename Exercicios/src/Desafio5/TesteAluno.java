189d42db7f476f00501619573d19ed3deb7571e7
YgorRodrigues <ygorrodrigues173@gmail.com>
2021-02-16 08:26:08 -0300

    Exercicios

---
+++ Exercicios/src/Desafio5/TesteAluno.java
@@ -0,0 +1,17 @@
+package Desafio5;
+
+public class TesteAluno {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno(1919099,"Ygor Jose Oliveira Rodrigues");
+		Notas n1 = new Notas(7.5, 7.0, 9.0, 8.0);
+
+		
+		
+		a1.exibir();
+		n1.mostrar();
+		
+
+	}
+
+}
