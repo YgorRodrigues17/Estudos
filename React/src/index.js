d62ba75abe0b68c0ac271de29f022c01f1a829f6
Ygor Jose Oliveira Rodrigues <ygorrodrigues173@gmail.com>
2021-08-06 08:29:46 -0300

    React

---
+++ React/src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import react from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+
+ReactDOM.render(
+    <Primeiro></Primeiro>,
+    document.getElementById('root')
+)
